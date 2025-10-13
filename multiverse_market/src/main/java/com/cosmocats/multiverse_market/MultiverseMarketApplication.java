cf8a716243b3516cbb903b6e497f175c2ef8331d
lady Maria <159808384+maari88@users.noreply.github.com>
2025-10-13 11:02:33 +0300

    Create MultiverseMarketApplication.java

---
+++ multiverse_market/src/main/java/com/cosmocats/multiverse_market/MultiverseMarketApplication.java
@@ -0,0 +1,13 @@
+package com.cosmocats.multiverse_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiverseMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiverseMarketApplication.class, args);
+	}
+
+}
