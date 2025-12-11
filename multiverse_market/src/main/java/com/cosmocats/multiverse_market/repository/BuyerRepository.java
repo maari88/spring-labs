6c3d4bb574a2f96fdf55fca560434215502afa51
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-11 12:13:09 +0200

    Update BuyerRepository.java

--- multiverse_market/src/main/java/com/cosmocats/multiverse_market/repository/BuyerRepository.java
+++ multiverse_market/src/main/java/com/cosmocats/multiverse_market/repository/BuyerRepository.java
@@ -1,14 +1,12 @@
 package com.cosmocats.multiverse_market.repository;
 
 import com.cosmocats.multiverse_market.model.Buyer;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface BuyerRepository {
     List<Buyer> findAll();
-    Optional<Buyer> findById(String id);
+    Optional<Buyer> findById(Long id);
     Buyer save(Buyer b);
-    void deleteById(String id);
+    void deleteById(Long id);
 }
-
