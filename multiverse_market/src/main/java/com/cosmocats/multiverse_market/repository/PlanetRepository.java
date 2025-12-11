82a7ff8b23ff7b71b506384068d9bc141ce8b4fd
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-11 12:13:16 +0200

    Update PlanetRepository.java

--- multiverse_market/src/main/java/com/cosmocats/multiverse_market/repository/PlanetRepository.java
+++ multiverse_market/src/main/java/com/cosmocats/multiverse_market/repository/PlanetRepository.java
@@ -1,14 +1,12 @@
 package com.cosmocats.multiverse_market.repository;
 
 import com.cosmocats.multiverse_market.model.Planet;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface PlanetRepository {
     List<Planet> findAll();
-    Optional<Planet> findById(String id);
+    Optional<Planet> findById(Long id);
     Planet save(Planet p);
-    void deleteById(String id);
+    void deleteById(Long id);
 }
-
