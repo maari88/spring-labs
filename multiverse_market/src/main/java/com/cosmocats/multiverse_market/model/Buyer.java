b63888043385de18cce8334eac8e7bdf6809de51
lady Maria <159808384+maari88@users.noreply.github.com>
2025-12-12 12:43:48 +0200

    Update Buyer.java

--- multiverse_market/src/main/java/com/cosmocats/multiverse_market/model/Buyer.java
+++ multiverse_market/src/main/java/com/cosmocats/multiverse_market/model/Buyer.java
@@ -1,8 +1,19 @@
 package com.cosmocats.multiverse_market.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "BUYERS")
 public class Buyer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "buyer_id")
     private Long id;
+
+    @Column(nullable = false)
     private String username;
+
     private String email;
 
     public Buyer() {}
@@ -18,27 +29,12 @@ public class Buyer {
         this.email = email;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
